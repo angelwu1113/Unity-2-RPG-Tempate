58d51547cbdfe6f8e20cf591b79b8d46de3d8133
admin <admin@admindeMacBook-Pro.local>
2020-11-18 18:45:32 -0800

    RPG

---
+++ Assets/Main/Scripts/ScriptableObjectsCode/Dialogue/DialogueBranch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Branch", menuName="CustomObject/Branch")]
+public class DialogueBranch : ScripatbleObject
+{
+    public string dialogueId = "";
+    public List<string> DialogueLines;
+    public List<ResponseBranch> ResponseOption;
+}
+
+[System.Serializabble]
+public class ResponseBranch
+{
+    public string text;
+    public DialogueBranch nextbranch;
+}
